208111bfefcf9e83f7c0420c178c5bc8056246ad
Benojan <skoj@qq.com>
2016-02-14 22:49:08 +0800

    Create split.h

---
+++ split.h
@@ -0,0 +1,9 @@
+#ifndef __beno_split__
+#define __beno_split__
+#include <iostream>
+#include <vector>
+using namespace std;
+
+void split(const wstring& src, const wstring& sep, vector<wstring>& des);
+
+#endif
